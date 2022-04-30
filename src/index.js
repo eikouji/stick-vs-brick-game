bcc5468777d4847379a8d8edbdc6cf9916822320
Eiko (A-koe) <91100425+eikouji@users.noreply.github.com>
2022-04-30 12:28:54 -0700

    added time to the stick

--- src/index.js
+++ src/index.js
@@ -12,3 +12,19 @@ ctx.clearRect(0, 0, 800, 600);
 let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
 
 paddle.draw(ctx);
+
+let lastTime = 0;
+
+function gameLoop(timeStamp) {
+  let deltaTime = timeStamp - lastTime;
+
+  lastTime = timeStamp;
+
+  ctx.clearRect(0, 0, 800, 600);
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
--- src/paddle.js
+++ src/paddle.js
@@ -1,7 +1,7 @@
 export default class Paddle {
   constructor(gameWidth, gameHeight) {
-    this.width = 150;
-    this.height = 30;
+    this.width = 170;
+    this.height = 25;
 
     this.position = {
       x: gameWidth / 2 - this.width / 2,
@@ -10,6 +10,11 @@ export default class Paddle {
   }
 
   draw(ctx) {
+    ctx.fillStyle = "#0ff";
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update(deltaTime) {
+    if (!deltaTime) return;
   }
 }
